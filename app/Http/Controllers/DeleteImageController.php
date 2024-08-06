26486fa18e307f211d0783fff46d317ca7126135
fran gregori <frangregori61@gmail.com>
2024-08-06 20:00:34 +0200

    Implementado el poder eliminar una imagen subida, se elimina en local y en la bd

--- app/Http/Controllers/DeleteImageController.php
+++ app/Http/Controllers/DeleteImageController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Image;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class DeleteImageController extends Controller
 {
+    public function __invoke(Request $request)
+    {
+        $name = $request->name;
+        $image = Image::where('name', $name)->first();
 
+        $directory = $image->path;
+        Storage::deleteDirectory($directory);
+
+        $image->delete();
+    }
 }
